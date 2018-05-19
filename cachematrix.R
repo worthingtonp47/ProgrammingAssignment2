cbf5c36dbadbe6f3945fdaee4bb11f43c5765088
worthingtonp47 <38982705+worthingtonp47@users.noreply.github.com>
2018-05-19 18:05:07 -0500

    Added my comments & code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The project creates a matrix from the given inputs. It moves on the calculate the
+## inverse of the matrix and store that calculation in memory. The next time the project
+##is called it searches the memory for a cached value before going through the process
+## of calculating the inverse matrix.
 
-## Write a short comment describing this function
+## This code is the cache, or "save to memory" piece of the project
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This piece of code is doing the heavy lifting of the actual solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  mat1 <- x$get()
+  s <- solve(mat1, ...)
+  x$setsolve(s)
+  s
 }
